9c0a3810c1e247348efd3598646a54750107df08
Esprees0 <kornstonk@gmail.com>
2024-08-02 00:11:12 +0700

    lazy

---
+++ TestHW/Testhw1-1.js
@@ -0,0 +1,16 @@
+function missingDigits(numStr){
+    const allnum = "0123456789";
+    const missdigit = {};
+    for (const i in allnum){
+        for (const j in numStr){
+            
+        }
+    }
+
+}
+
+  // Call the function with some test cases and log the results
+  console.log(missingDigits("123456789")); // 123456789 Missing Digit = 0
+  console.log(missingDigits("0123456789")); // 0123456789 has no missing digits
+  console.log(missingDigits("13579")); // 13579 Missing Digit = 0, 2, 4, 6, 8
+ 
